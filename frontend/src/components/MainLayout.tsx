4f4451e9764a4d7b87bf321f18e7eace311a4e3f
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-05-06 15:24:09 +0200

    feat: start finalising future menu items layout

--- frontend/src/components/MainLayout.tsx
+++ frontend/src/components/MainLayout.tsx
@@ -1,20 +1,33 @@
 import { Outlet, Link } from 'react-router';
 import '../styles/mainLayout.css';
+import { useState } from 'react';
 
 const MainLayout = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   return (
     <div className="main-layout">
-      <nav>
-        <Link to="/" className="heading">
+      <nav className="navigation">
+        <Link to="/" className="logo">
           FuelShare
         </Link>
 
-        <Link to="/" className="heading">
-          Single Trip
-        </Link>
+        {/* Hamburger only visible on mobile */}
+        <button className="hamburger-button" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
+        </button>
+
+        {/* Nav links container - changes style based on screen size */}
+        <div className={`nav-links ${menuOpen ? 'is-open' : ''}`}>
+          <Link to="/">Single Trip</Link>
+        </div>
+
+        {/* Settings */}
+        <div className="settings">Settings</div>
+        <div className="">Logo</div>
       </nav>
 
       <Outlet />
+
       <footer className="footer">
         <hr /> All rights reserved
       </footer>
