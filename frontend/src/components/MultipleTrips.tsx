19bc1c2b7734df9dd9023505fe393bf661379d37
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-05-07 14:52:01 +0200

    feat: add filler text for multiple trips tab

--- frontend/src/components/MultipleTrips.tsx
+++ frontend/src/components/MultipleTrips.tsx
@@ -1,4 +1,11 @@
+import '../styles/MultipleTrips.css';
+
 const MultipleTrips = () => {
-  return <h1>This is Multiple Trips</h1>;
+  return (
+    <div id="main-body-container-multiple-trips">
+      <h1>ERROR 404:</h1>
+      <p>Exciting things will be here soon!</p>
+    </div>
+  );
 };
 export default MultipleTrips;
