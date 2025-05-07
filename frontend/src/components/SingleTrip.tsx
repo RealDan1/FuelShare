689551e2c1a923880b0b4fc203ea5fceb3313ee5
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2025-05-07 14:07:46 +0200

    feat: add base single trip calculator form

--- frontend/src/components/SingleTrip.tsx
+++ frontend/src/components/SingleTrip.tsx
@@ -1,4 +1,24 @@
+import '../styles/singleTrip.css';
+
 const SingleTrip = () => {
-  return <h1>This is SingleTrip</h1>;
+  return (
+    <div id="main-body-container">
+      <h1>Fuel Share Calculator</h1>
+      <div id="calculator-container">
+        <form id="fuel-calculator-form">
+          <label htmlFor="distance">Distance (km):</label>
+          <input type="number" id="distance" name="distance" required />
+
+          <label htmlFor="fuel-efficiency">Fuel Efficiency (L/100km):</label>
+          <input type="number" id="fuel-efficiency" name="fuel-efficiency" required />
+
+          <label htmlFor="fuel-price">Fuel Price (per L):</label>
+          <input type="number" id="fuel-price" name="fuel-price" required />
+
+          <button type="submit">Calculate</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default SingleTrip;
